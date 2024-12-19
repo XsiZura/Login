6a026b11209e78dddc715a4b3d494547f55a4475
XsiZura <rizasaktiawanmoreno@gmail.com>
2024-12-19 15:33:10 +0700

    modifikasi blocked.php

--- application/views/auth/blocked.php
+++ application/views/auth/blocked.php
@@ -38,9 +38,9 @@
 
                     <!-- 404 Error Text -->
                     <div class="text-center">
-                        <div class="error mx-auto" data-text="404">404</div>
+                        <div class="error mx-auto" data-text="403">403</div>
                         <p class="lead text-gray-800 mb-5">Access Forbidden</p>
-                        <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
+                        <p class="text-gray-500 mb-0">Kamu Tidak Memiliki Akses Ke Halaman Ini 🚫🤚</p>
                         <a href="<?= base_url('user')?>">&larr; Back to Dashboard</a>
                     </div>
 
